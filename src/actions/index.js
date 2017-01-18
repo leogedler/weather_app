53f4b0ec826900899365af2ee33cc1f302fad7dd
leonardogedler <leogedler@hotmail.com>
2017-01-17 22:42:22 -0400

    Ajax request using axios

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_KEY = '5d1462ea9c1fc173bdcf732898557236';
+const ROUTE_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+    const url = `${ROUTE_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    console.log('Request: ', request);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
