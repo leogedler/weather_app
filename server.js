ab5c46ca7af9ebc2f46304718c356a04ffda8a00
leonardogedler <leogedler@hotmail.com>
2018-02-13 20:24:03 -0400

    Changed server configuration

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const express = require('express')
 module.exports = {
   app: function () {
     const app = express()
-    const indexPath = path.join(__dirname, '/index.html')
-    const publicPath = express.static(path.join(__dirname, '../public'))
+    const indexPath = path.join(__dirname, 'index.html')
+    const publicPath = express.static(path.join(__dirname, 'public'))
 
     // app.use(express.static(__dirname + '/public'));
     // app.get('/', function(request, response) {
